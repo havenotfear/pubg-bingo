94e8182b3e1ea7cc57a55caf1e52fe5dce90a593
Jason Coakley <jason.coakley@openroadsconsulting.com>
2018-10-11 15:17:27 -0400

    Update responsivness

--- src/app/room/room.page.ts
+++ src/app/room/room.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from '../../pubg-bingo-types';
+import { Route, ActivatedRoute } from '@angular/router';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-room',
@@ -6,8 +13,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['room.page.scss']
 })
 export class RoomPage implements OnInit {
-  constructor() { }
+  private roomDoc: AngularFirestoreDocument<Room>;
+  room: Observable<Room>;
+
+  constructor(private route: ActivatedRoute, private afs: AngularFirestore) {}
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.roomDoc = this.afs.doc<Room>(`rooms/${id}`);
+    this.room = this.roomDoc.valueChanges();
   }
 }
